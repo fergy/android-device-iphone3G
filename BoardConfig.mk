6bd3776bcb501dbee4610560e1bcdec09280393d
fergy <ramon.rebersak@gmail.com>
2013-03-25 17:06:01 +0100

    Clean-up of build env.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,7 @@ BUILD_WITH_ALSA_UTILS := true
 
 BOARD_USES_LIBSECRIL_STUB := true
 
+#TODO: fix this for Z2 and other closed parts
 # Use the non-open-source parts, if they're present
 #-include vendor/apple/iphone3G/BoardConfigVendor.mk
 
@@ -41,7 +42,7 @@ TARGET_NO_RADIOIMAGE := true
 TARGET_PROVIDES_INIT := true
 TARGET_PROVIDES_INIT_RC := true
 TARGET_PROVIDES_INIT_TARGET_RC := true
-#TODO:
+#TODO: implement of recovery image
 #TARGET_BOARD_PLATFORM := s5l8900
 #TARGET_BOOTLOADER_BOARD_NAME := IPHONE3G
 #TARGET_RECOVERY_INITRC := device/apple/iphone3G/common/recovery.rc
@@ -49,9 +50,10 @@ TARGET_PROVIDES_MEDIASERVER := true
 
 BOARD_MOBILEDATA_INTERFACE_NAME = "pdp0"
 
+#TODO:
 # Releasetools
-#TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/apple/common/releasetools/c1_ota_from_target_files
-#TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/apple/common/releasetools/c1_img_from_target_files
+#TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/apple/common/releasetools/iphone3g_ota_from_target_files
+#TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/apple/common/releasetools/iphone3g_img_from_target_files
 
 # Camera
 USE_CAMERA_STUB := true
@@ -67,14 +69,16 @@ BOARD_HAVE_BLUETOOTH_BCM := true
 # gps
 BOARD_USES_GPSWRAPPER := true
 
+#TODO: if We reprogram chip and enable functions...
 # FM Radio
 #BOARD_HAVE_FM_RADIO := true
 #BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
-#BOARD_FM_DEVICE := si4709
+#BOARD_FM_DEVICE := ?
 
 # Vibrator
 BOARD_HAS_VIBRATOR_IMPLEMENTATION := device/apple/iphone3G/vibrator/vibrator.c
 
+#TODO: fix this for Kernel build inside B2G
 #BOARD_NAND_PAGE_SIZE := 4096 -s 128
 #BOARD_NAND_PAGE_SIZE := 4096
 #BOARD_KERNEL_PAGESIZE := 4096
@@ -82,16 +86,21 @@ BOARD_HAS_VIBRATOR_IMPLEMENTATION := device/apple/iphone3G/vibrator/vibrator.c
 #BOARD_KERNEL_CMDLINE := console=ttySAC2,115200 consoleblank=0
 #TARGET_PREBUILT_KERNEL := device/apple/iphone3G/prebuilt/kernel
 
-#BOARD_BOOTIMAGE_PARTITION_SIZE := 104857600
+TARGET_BOOTIMAGE_USE_EXT4 := true
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
+
+#TODO: boot (openiboot) partition size for complete removing iBoot
+#BOARD_BOOTIMAGE_PARTITION_SIZE := ?
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 104857600
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 104857600
 BOARD_FLASH_BLOCK_SIZE := 4096
 
 # Connectivity - Wi-Fi
 BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+#TODO: implement this
 #WPA_SUPPLICANT_VERSION := VER_0_8_X
-#BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-#BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+#BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_?
+#BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_?
 #BOARD_WLAN_DEVICE := libertas_sdio
 #WIFI_DRIVER_MODULE_PATH := "/lib/modules/libertas_sdio.ko"
 #WIFI_DRIVER_MODULE_NAME := "libertas_sdio"
@@ -102,14 +111,13 @@ BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
 # Recovery
 TARGET_USERIMAGES_USE_EXT4 := true
+#TODO: when (one day) recovery stuff will be added, use custom mappings
 #BOARD_CUSTOM_RECOVERY_KEYMAPPING := device/apple/iphone3G/recovery/recovery_ui.c
 #BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_MISC_PARTITION := true
 
+#TODO
 #BOARD_CUSTOM_BOOTIMG_MK := device/apple/iphone3G/common/shbootimg.mk
 
 # assert
 TARGET_OTA_ASSERT_DEVICE := IPHONE3G
-
-# Include aries specific stuff
-#-include device/apple/iphone3G/common/Android.mk
